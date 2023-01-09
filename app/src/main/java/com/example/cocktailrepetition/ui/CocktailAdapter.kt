24e22290a21fd532574adc66f31ecba415af3265
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2023-01-09 09:47:03 +0000

    CocktailAdapter.kt soweit es ging, brauchen aber unsere data class Cocktail, die ein item im Adapter darstellen soll

---
+++ app/src/main/java/com/example/cocktailrepetition/ui/CocktailAdapter.kt
@@ -0,0 +1,38 @@
+package com.example.cocktailrepetition.ui
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cocktailrepetition.R
+import com.example.cocktailrepetition.data.models.Cocktail
+
+class CocktailAdapter(): RecyclerView.Adapter<CocktailAdapter.ItemViewHolder>() {
+
+    private var dataset = listOf<Cocktail>()
+
+    inner class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(
+        view){
+        val name = view.findViewById<TextView>(R.id.cocktail_name)
+        val picture = view.findViewById<ImageView>(R.id.cocktail_image)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
+        val adapterLayout = LayoutInflater.from(parent.context)
+            .inflate(R.layout.cocktail_item, parent, false)
+        return ItemViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
+        val item = dataset[position]
+        //TODO: set name and picture, need Cocktail data class for that
+    }
+
+    override fun getItemCount(): Int {
+        return dataset.size
+    }
+
+
+}
