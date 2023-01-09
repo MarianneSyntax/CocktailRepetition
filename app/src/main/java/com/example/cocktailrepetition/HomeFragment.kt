cc8dd6886814c536ed14beb3dfa54116d4b9bf79
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2023-01-09 10:38:28 +0000

    HomeFragment.kt so weit es geht angefangen, braucht jetzt Daten vom ViewModel -> das braucht daten vom repository -> das holt sich daten aus dem api service und schreibt sie in die lokale Datenbank. deshalb als nächstes: der ApiService

---
+++ app/src/main/java/com/example/cocktailrepetition/HomeFragment.kt
@@ -0,0 +1,40 @@
+package com.example.cocktailrepetition
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.example.cocktailrepetition.databinding.FragmentHomeBinding
+import com.example.cocktailrepetition.ui.CocktailAdapter
+
+class HomeFragment : Fragment() {
+
+
+    private val viewModel: HomeViewModel by viewModels()
+
+    private lateinit var binding: FragmentHomeBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val cocktailAdapter = CocktailAdapter()
+        val cocktailRecycler = binding.cocktailRecycler
+        cocktailRecycler.adapter = cocktailAdapter
+
+        // todo: viewModel, wo die daten herkommen, benutzen
+
+    }
+
+
+}
