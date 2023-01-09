85e70b8485b1701614eb55e9966efc9ad8f7e5ae
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2023-01-09 09:51:44 +0000

    CocktailAdapter.kt vorerst fertig

--- app/src/main/java/com/example/cocktailrepetition/ui/CocktailAdapter.kt
+++ app/src/main/java/com/example/cocktailrepetition/ui/CocktailAdapter.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
 import com.example.cocktailrepetition.R
 import com.example.cocktailrepetition.data.models.Cocktail
 
@@ -16,7 +17,7 @@ class CocktailAdapter(): RecyclerView.Adapter<CocktailAdapter.ItemViewHolder>() 
     inner class ItemViewHolder(private val view: View): RecyclerView.ViewHolder(
         view){
         val name = view.findViewById<TextView>(R.id.cocktail_name)
-        val picture = view.findViewById<ImageView>(R.id.cocktail_image)
+        val image = view.findViewById<ImageView>(R.id.cocktail_image)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
@@ -28,10 +29,18 @@ class CocktailAdapter(): RecyclerView.Adapter<CocktailAdapter.ItemViewHolder>() 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
         //TODO: set name and picture, need Cocktail data class for that
+        holder.name.text = item.cocktailName
+        // load kommt von coil
+        holder.image.load(item.cocktailImage)
     }
 
     override fun getItemCount(): Int {
         return dataset.size
+    }
+
+    fun submitList(list: List<Cocktail>) {
+        dataset = list
+        notifyDataSetChanged()
     }
 
 
