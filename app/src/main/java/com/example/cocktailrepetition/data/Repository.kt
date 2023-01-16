fee2f33aa7aa2622af665cf917f540bcf24d38cd
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2023-01-16 13:48:13 +0000

    rufe getMargaritaList aus dem ApiService im repo auf und gebe das Ergebnis zurück.

--- app/src/main/java/com/example/cocktailrepetition/data/Repository.kt
+++ app/src/main/java/com/example/cocktailrepetition/data/Repository.kt
@@ -1,8 +1,18 @@
 package com.example.cocktailrepetition.data
 
+import com.example.cocktailrepetition.data.models.Cocktail
+import com.example.cocktailrepetition.data.remote.CocktailApi
+
 
 class Repository() {
 
-    //todo: suspend function, die die cocktails aus dem api call holt
+    //suspend function, die die cocktails aus dem api call holt
+
+    suspend fun getCocktails() : List<Cocktail>{
+        // CocktailList -> List<Cocktails> in ner Variable namens drinks
+        val result = CocktailApi.retrofitService.getMargaritaList().drinks
+        return result
+    }
+
 
 }
