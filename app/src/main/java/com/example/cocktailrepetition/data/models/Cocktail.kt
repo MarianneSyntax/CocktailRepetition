ed7948d49f6fd78a20d28b4503c4c6761725ebea
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2023-01-16 13:33:11 +0000

    gradle vorbereitet,
    data models erstellt,
    
    json antwort beispiel eingefügt

--- app/src/main/java/com/example/cocktailrepetition/data/models/Cocktail.kt
+++ app/src/main/java/com/example/cocktailrepetition/data/models/Cocktail.kt
@@ -1,5 +1,16 @@
 package com.example.cocktailrepetition.data.models
 
 import androidx.room.PrimaryKey
+import com.squareup.moshi.Json
 
 //todo: data class, die die antwort des api calls auffängt
+
+data class Cocktail(
+
+    @Json(name = "strDrink")
+    val cocktailName: String,
+
+    @Json(name = "strDrinkThumb")
+    val cocktailImage: String
+
+)
--- app/src/main/java/com/example/cocktailrepetition/data/models/CocktailList.kt
+++ app/src/main/java/com/example/cocktailrepetition/data/models/CocktailList.kt
@@ -1,3 +1,7 @@
 package com.example.cocktailrepetition.data.models
 
 //todo: data class, die die antwort des api calls auffängt
+
+data class CocktailList(
+    val drinks: List<Cocktail>
+)
