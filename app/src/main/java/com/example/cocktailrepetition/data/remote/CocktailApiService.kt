3088c95bebb43ebadac7cabc4e221b6417d32409
MarianneSyntax <113111255+MarianneSyntax@users.noreply.github.com>
2023-01-16 13:42:29 +0000

    feat(ure): api service impl(ementiert)

--- app/src/main/java/com/example/cocktailrepetition/data/remote/CocktailApiService.kt
+++ app/src/main/java/com/example/cocktailrepetition/data/remote/CocktailApiService.kt
@@ -1,15 +1,46 @@
 package com.example.cocktailrepetition.data.remote
 
-//todo: imports
+import com.example.cocktailrepetition.data.models.CocktailList
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.DELETE
+import retrofit2.http.GET
 
 
-//todo: base url
+//base url
 
-// todo: moshi converter factory
+const val BASE_URL = "www.thecocktaildb.com/api/json/v1/1/"
 
-//todo: retrofit builder
+//moshi converter factory
 
-//todo: interface mit get function
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
 
-//todo: object, das interface implementiert
+//retrofit builder
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+//interface mit get function
+
+interface CocktailApiService{
+
+    @GET("search.php?s=margarita")
+    suspend fun getMargaritaList() : CocktailList
+
+}
+
+
+//object, das interface implementiert
+
+object CocktailApi {
+    val retrofitService: CocktailApiService by lazy {
+        retrofit.create(CocktailApiService::class.java)
+    }
+}
 
